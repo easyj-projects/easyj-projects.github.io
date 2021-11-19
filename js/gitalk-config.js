e70c67ae4207947d4040a4cd5e96e5fc44063e97
王良 <841369634@qq.com>
2021-11-19 17:26:22 +0800

    添加gitalk-config.js文件

---
+++ js/gitalk-config.js
@@ -0,0 +1,8 @@
+// 插件：评论系统
+const gitalk = new Gitalk({
+	clientID: '21bffd940d486618132b',
+	clientSecret: '5b51c3a75de223a4ce17664320d8243689b3da9f',
+	repo: communityHomePageProjectName,
+	owner: communityName,
+	admin: ['wangliang181230']
+});
