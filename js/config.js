d74af214b798762d339cfbf1d325aa59c71eecc0
王良 <841369634@qq.com>
2021-12-08 09:48:20 +0800

    合并配置文件

--- js/config.js
+++ js/config.js
@@ -1,4 +1,11 @@
-// 插件：评论系统配置
+// 站点配置
+var config = {
+	communityName: 'easyj-projects', // 社区名称/项目组名称
+	projectName: 'easyj-projects.github.io', // 项目名
+	branchName: 'docsify' // 项目分支名
+};
+
+// 插件：Gitalk（评论系统）配置
 window.gitalkConfig = {
 	clientID: '21bffd940d486618132b',
 	clientSecret: '5b51c3a75de223a4ce17664320d8243689b3da9f',
@@ -6,6 +13,5 @@ window.gitalkConfig = {
 	owner: config.communityName,
 	admin: ['wangliang181230']
 };
-
 // 创建gitalk实例
 //window.gitalk = new Gitalk(window.gitalkConfig); // 因为在重写的 `docsify-plugins-gitalk.js` 文件中有创建对象，所以这里无需创建对象
