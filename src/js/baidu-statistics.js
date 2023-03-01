b02ba91fb8c6ce0c56c6a0d5ea0ad572b0fc4ddf
王良 <841369634@qq.com>
2023-03-01 15:10:30 +0800

    开发环境下，不加载百度统计。

--- src/js/baidu-statistics.js
+++ src/js/baidu-statistics.js
@@ -1,8 +1,10 @@
 // 百度统计
-const _hmt = window._hmt || [];
-(function (d) {
-	const hm = d.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?efa7b04534aa7088ff0e2106757375b3";
-	const s = d.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-})(document);
+if (!window.config || window.config.env !== "local") {
+	(function (w, d) {
+		w._hmt = w._hmt || [];
+		const hm = d.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?efa7b04534aa7088ff0e2106757375b3";
+		const s = d.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})(window, document);
+}
